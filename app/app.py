3250f2286bb8154c0185ed96a994d82455ff23c6
hectorsp10 <sanchezpendas2000@gmail.com>
2024-06-19 08:22:05 +0200

    project added

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request, g
+import sqlite3
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+database = 'healthy.db'
+
+#database connection
+def get_db():
+    db = getattr(g, '_healthy', None)
+    if db is None:
+        db = g._healthy = sqlite3.connect(database)
+    return db
+
+@app.route('/')
+def index():
+    return render_template('layout.html')
+
+if __name__ == '_main_':
+    app.run(debug=True)
